251e6856ea7ea4f7bda8dc31fb0827b034a012e6
Devikrishna <itsmedevi96@gmail.com>
2025-04-11 23:01:43 +0530

    User layout Check user updation

--- Frontend/src/layout/Userlayout.jsx
+++ Frontend/src/layout/Userlayout.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from 'react'
 import Header from '../components/user/Header'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Footer } from '../components/user/Footer'
 import { UserHeader } from '../components/user/UserHeader'
+import { useDispatch } from 'react-redux'
+import { clearUser } from '../redux/features/userSlice'
+import { axiosInstance } from '../config/axiosInstance'
 
 function Userlayout() {
+  const user=useSelector((state)=>state.user)
   const [isUserAuth,setIsUserAuth]=useState(true)
-  return (
+
+  const dispatch=useDispatch();
+  const location=useLocation();
+  const checkUser=async()=>{
+    try {
+      const response=await axiosInstance({method:"GET",url:"/user/check-user"})
+      dispatch(saveUser())
+    } catch (err) {
+      console.log(err);
+      dispatch(clearUser());
+      
+    }
+  }
+  useEffect(()=>{
+    checkUser()
+    },[location.pathname])
+  
+  return isLoading ? null :(
     <div>
    {isUserAuth ? <UserHeader/> :<Header/>}
    <div className='min-h-96'>
